2c8c600b4a1f260e4d2eee8ded6042e483bb1214
Ian Monroe <ian@ianmonroe.com>
2017-08-27 10:45:00 -0500

    updated the installation script

--- cr_configure.php
+++ cr_configure.php
@@ -4,5 +4,19 @@
 	Post installation configuration script for coldreader
 */
 
-echo 'beginning coldreader post-installation script';
+function execute($cmd){
+	echo 'Executing '.$cmd.PHP_EOL;
+	echo(shell_exec($cmd));
+}
+
+echo 'beginning coldreader post-installation script'.PHP_EOL;
+
+execute('composer update');
+execute('php artisan migrate');
+execute('php artisan vendor:publish');
+execute('php artisan storage:link');
+execute('php artisan vendor:publish');
+
+echo 'completed post-installation script.  enjoy the software!';
+
 exit();
