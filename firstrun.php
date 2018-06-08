a0762205be716cc6027a9d59fd29c272181067ea
Ian Monroe <ian@ianmonroe.com>
2018-06-07 21:00:22 -0500

    moved installer to console command

--- firstrun.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $msg = PHP_EOL.PHP_EOL;
-  $msg .= 'Thanks for trying out Coldreader!'.PHP_EOL;
-  $msg .= 'To complete the installation, please run the following command: '.PHP_EOL;
-  $msg .= 'php cr_configure.php'.PHP_EOL.PHP_EOL;
-  $msg .= 'You will be prompted for your database credentials, and the application will set itself up.'.PHP_EOL;
-  echo($msg);
-?>
