99840c5929a0379aa1f2a99964c062868122f944
Ian Monroe <ian@ianmonroe.com>
2018-06-16 21:25:03 -0500

    tinker

--- firstrun.php
+++ firstrun.php
@@ -3,28 +3,12 @@
   
 
   function run_process( $cmd){
-    $descriptorspec = array(
-        0 => array("pipe", "r"), 
-        1 => array("pipe", "w"), 
-        2 => array("pipe", "r")
+    $descriptors = array(
+      array('file', '/dev/tty', 'r'),
+      array('file', '/dev/tty', 'w'),
+      array('file', '/dev/tty', 'w')
     );
-    $process = proc_open($cmd, $descriptorspec, $pipes, null, null); //run test_gen.php
-    echo ("Start process:\n");
-    if (is_resource($process)) 
-    {
-        fwrite($pipes[0], "start\n");    // send start
-        echo ("\n\nStart ....".fgets($pipes[1],4096)); //get answer
-        fwrite($pipes[0], "get\n");    // send get
-        echo ("Get: ".fgets($pipes[1],4096));    //get answer
-        fwrite($pipes[0], "stop\n");    //send stop
-        echo ("\n\nStop ....".fgets($pipes[1],4096));  //get answer
-    
-        fclose($pipes[0]);
-        fclose($pipes[1]);
-        fclose($pipes[2]);
-        $return_value = proc_close($process);  //stop test_gen.php
-        echo ("Returned:".$return_value."\n");
-    }
+    $process = proc_open($cmd, $descriptors, $pipes);
   }
 
   $msg = PHP_EOL.PHP_EOL;
