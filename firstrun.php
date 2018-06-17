0f192004d7f763f67a6753bf908a334fb1d067f4
Ian Monroe <ian@ianmonroe.com>
2018-06-16 20:31:03 -0500

    installation with git needs composer, but installation with composer does not.

--- firstrun.php
+++ firstrun.php
@@ -2,5 +2,8 @@
   $msg = PHP_EOL.PHP_EOL;
   $msg .= 'Thanks for trying out Coldreader!'.PHP_EOL;
   echo($msg);
-  exec('composer install --no-dev --no-suggest');
+  if (!file_exists('./vendor/imonroe/ana/src/Ana.php')){
+    echo('Looks like you installed via git. First we will get the composer dependencies.'.PHP_EOL.PHP_EOL);
+    exec('composer install --no-dev --no-suggest');
+  }
 ?>
