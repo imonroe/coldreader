7433a6608dd7cb097c6483eb2c8c465e6c0756f6
Ian Monroe <ian@ianmonroe.com>
2018-06-16 20:43:08 -0500

    tweak

--- firstrun.php
+++ firstrun.php
@@ -4,7 +4,7 @@
   echo($msg);
   if (!file_exists('./vendor/imonroe/ana/src/Ana.php')){
     echo('Looks like you installed via git. First we will get the composer dependencies.'.PHP_EOL.PHP_EOL);
-    exec('composer install --no-dev --no-suggest');
+    shell_exec('composer install --no-dev --no-suggest');
   }
   shell_exec('php artisan coldreader:install');
 ?>
