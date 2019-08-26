f7f7068f58e66d2aea7a28e77cc49a0471858c9b
Ian Monroe <ian@ianmonroe.com>
2019-08-26 16:28:19 -0600

    some good updates.

--- application/public/js/coldreader.js
+++ application/public/js/coldreader.js
@@ -1,1 +1,197 @@
-!function(e){var t={};function n(i){if(t[i])return t[i].exports;var o=t[i]={i:i,l:!1,exports:{}};return e[i].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,i){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:i})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=413)}({413:function(e,t,n){e.exports=n(414)},414:function(e,t){tinymce.init({selector:".wysiwyg_editor",height:500,theme:"modern",plugins:["advlist autolink lists link image charmap print preview hr anchor pagebreak","searchreplace wordcount visualblocks visualchars code fullscreen","insertdatetime media nonbreaking save table contextmenu directionality","emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help"],toolbar1:"undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",content_css:["//fonts.googleapis.com/css?family=Lato:300,300i,400,400i"]})}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 437);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 437:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(438);
+
+
+/***/ }),
+
+/***/ 438:
+/***/ (function(module, exports) {
+
+// Let's make globally accessible JS go here.
+
+
+// Initialize any WYSIWYG components on the page.
+tinymce.init({
+  selector: '.wysiwyg_editor',
+  height: 500,
+  theme: 'modern',
+  plugins: ['advlist autolink lists link image charmap print preview hr anchor pagebreak', 'searchreplace wordcount visualblocks visualchars code fullscreen', 'insertdatetime media nonbreaking save table contextmenu directionality', 'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'],
+  toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  content_css: ['//fonts.googleapis.com/css?family=Lato:300,300i,400,400i']
+});
+
+/**
+ * 
+ * 
+ *
+ * 
+ * // Support for Masonry.js
+      jQuery.rejigger = function(){
+      var Masonry = require('masonry-layout');
+      var gutter = (parseInt($('.aspect_display').css('marginBottom')) / 2 );
+      var container = document.querySelector('#grid');
+
+      if (!(container == null)){
+        var msnry = new Masonry( container, {
+          columnWidth: '.aspect_display',
+          gutter:gutter,
+          transitionDuration: '0.5s',
+          stagger: 30
+        });
+        msnry.reloadItems();
+        msnry.layout();
+        console.log('layout rejiggered.');
+      }
+      }
+ * 
+ * 
+ * 
+ * 
+ *  
+ * 
+
+
+// Here's our master JS on-load trigger.
+$(function(){
+  // We'll need access to the csrf token, so let's just load it up now.
+  let token = document.head.querySelector('meta[name="csrf-token"]');
+  if (token) {
+      var csrf_code = token.content;
+  } else {
+      console.error('CSRF token not found.');
+  }
+
+  jQuery.display_modal = function(modal_content){
+  	var modal_container = $("#modal-container");
+  	modal_container.html(modal_content);
+  	var dialog = modal_container.dialog({
+  			autoOpen: false,
+  			height: "auto",
+  			width:"auto",
+  			modal: true,
+  		    draggable: false
+  		});
+  	dialog.dialog( "open" );
+  }
+
+  // Now let's do some stuff.
+	// First things first, Let's set up our layout.
+	$.rejigger();
+
+	  // Anything that's marked with a confirmation class gets an alert to make sure that we know what we're doing.
+	  $('.confirmation').on('click', function () {
+        return confirm('Are you sure you want to do that?');
+    });
+
+
+
+  // Support for drag and drop reordering.
+  $(".sortable").sortable({
+		handle: '.reorder-handle',
+		placeholder: "ui-state-highlight",
+		update: function (event, ui) {
+			var data = $(this).sortable('serialize');
+			data = data + '&_token=' + csrf_code;
+			$.post( "/subject/aspect_sorter", data );
+			$.rejigger();
+		}
+	});
+
+
+	//Support for javascript geolocation
+	function get_geographic_coordinates(){
+		var current_location = [];
+		navigator.geolocation.getCurrentPosition(function(location) {
+			current_location["latitude"] = location.coords.latitude;
+			current_location["longitude"] = location.coords.longitude;
+			current_location["acccuracy"] = location.coords.accuracy;
+			document.cookie = "current_latitude="+current_location["latitude"];
+			document.cookie = "current_longitude="+current_location["longitude"];
+		});
+		return current_location;
+	}
+	//get_geographic_coordinates();
+
+	// Bind a listener to the window resize event, and rejigger the layout if needed.
+	jQuery(window).bind('resize', function () {
+		$.rejigger();
+	});
+
+
+});
+ 
+
+*/
+
+/***/ })
+
+/******/ });
