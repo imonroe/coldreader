09f8236d7ad308b613e9ae743aebedcb5d41d172
Ian Monroe <ian@ianmonroe.com>
2019-11-22 12:04:36 -0700

    updated to reflect that we don't need to rebuild everything every time we load the app container.

--- docker-entry.sh
+++ docker-entry.sh
@@ -8,12 +8,13 @@ else
 FIRST_RUN=false
 fi
 
+if [ $FIRST_RUN = true ]; then
 echo Installing Composer dependencies.
 composer install
 echo Completed Composer install.
 
 echo Checking out the environment.
-if [ $FIRST_RUN = true ]; then
+
 # we are running this app for the first time.
 echo This is a first run, so we will create the environment file.
 cp /var/www/.env.example /var/www/.env
@@ -24,7 +25,7 @@ echo Setting up cron.
 touch /etc/crontab /etc/cron.*/*
 crontab -l | { cat; echo "* * * * * cd /var/www && php artisan schedule:run >> /dev/null 2>&1"; } | crontab -
 echo Crontab added.
-fi
+
 echo Finished checking out the environment.
 
 echo Installing Node dependencies.
@@ -42,6 +43,8 @@ echo Database schema migrations complete.
 echo Clearing the Laravel cache.
 php artisan cache:clear
 echo Laravel cache cleared.
+
+fi
 
 echo Preflight completed. Have fun!
 php-fpm -D -R
